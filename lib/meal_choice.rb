2a5e26841ebb2300a80cff86aa5e9dfbfb597fa6
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-05 01:29:40 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,4 @@
 def meal_choice(veg1, veg2, protein="meat")
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return puts "A plate of #{protein} with #{veg1} and #{veg2}."
  end
 
-meal_choice(veg1, veg2, protein)
